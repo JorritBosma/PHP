b1a6a8b018c946067e2d18e593adcea0cc504fcb
JorritBosma <bosma.jorrit@gmail.com>
2020-12-16 20:27:53 +0100

    GET and POST routes untanglement 101.

--- core/Request.php
+++ core/Request.php
@@ -4,6 +4,16 @@ class Request
 {
     public static function uri()
     {
-        return trim($_SERVER['REQUEST_URI'], '/');
+        return trim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/');
+    }
+
+    public static function method()
+    {
+        return $_SERVER['REQUEST_METHOD'];
     }
 }
+
+// parse_url heeft meerdere methoden. 
+// PHP_URL_PATH geeft je alleen de path, dus niet de query. 
+// handig om naar je endpoint te komen.
+// Dat wrap je weer in de trim, zodat je de slash er ook nog af kunt schrapen, en klaar is Kees.
--- views/index.view.php
+++ views/index.view.php
@@ -1,22 +1,9 @@
 <?php require('partials/head.php'); ?>
 
-<h1>Doelijst
-    <?= $takke; ?>
-</h1>
-<ul>
-    <?php foreach ($taakjes as $taak) : ?>
+<h1>Vul je naam maar in hoor</h1>
 
-        <li>
-            <?php if ($taak->voltooid) : ?>
-                <s>
-                    <?= $taak->beschrijving ?>
-                </s>
-            <?php else : ?>
-
-                <?= $taak->beschrijving ?>
-            <?php endif ?>
-        </li>
-
-    <?php endforeach; ?>
-</ul>
+<form method="POST" action="/names">
+    <input name="name"></input>
+    <button type="submit">Indienen maar!</button>
+</form>
 <?php require('partials/footer.php'); ?>
